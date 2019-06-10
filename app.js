e916a9c2dac1318fc55ea275580c8672b6218458
Mulumba <amulumba@vodacom.cd>
2019-06-11 00:08:10 +0100

    Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // we should define this when using bcrypt
+//const md5 = require('md5');
+//const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -17,73 +21,91 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true
+});
 
 const userSchema = new mongoose.Schema({
-  email : String,
-  password : String
+  email: String,
+  password: String
 });
 
 //const secret = 'Thisisourlittlesecret.';
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});// Always before creating mongoose Model
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});// Always before creating mongoose Model
+
 
 const User = mongoose.model('User', userSchema);
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function(req, res) {
   res.render('login');
 });
 
-app.get('/register', function(req, res){
+app.get('/register', function(req, res) {
   res.render('register');
 });
 
-app.post('/register', function(req, res){
+app.post('/register', function(req, res) {
 
-  User.findOne({email : req.body.username}, function(err, userFound){
-    if(!err){
-      if(!userFound){
-        const newUser = new User({
-          email : req.body.username,
-          password : req.body.password
-        });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    User.findOne({
+      email: req.body.username
+    }, function(err, userFound) {
+      if (!err) {
+        if (!userFound) {
+          const newUser = new User({
+            email: req.body.username,
+            //password : md5(req.body.password)
+            password: hash // the hash password generated
+          });
 
-        newUser.save(function(err){
-          if(!err){
-            res.render('secrets');
-          } else {
-            console.log(err);
-          }
-        });
-      } else {
-        res.redirect('/login');
-      }
-    } else{
-      console.log(err);
-    }
-  });
-});
-
-app.post('/login', function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email : username}, function(err, userFound){
-    if(err){
-      console.log(err);
-    } else {
-      if(userFound){
-        if(userFound.password === password){
-          res.render('secrets');
+          newUser.save(function(err) {
+            if (!err) {
+              res.render('secrets');
+            } else {
+              console.log(err);
+            }
+          });
+        } else {
+          res.redirect('/login');
         }
       } else {
-        res.redirect('/login');
+        console.log(err);
       }
-    }
+    });
   });
+
+
+});
+
+app.post('/login', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+    User.findOne({
+      email: username
+    }, function(err, userFound) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (userFound) {
+          bcrypt.compare(password, userFound.password, function(err, result) {
+            if (result === true) {
+              res.render('secrets');
+            }else {
+              console.log('Not found');
+              res.redirect('/login');
+            }
+          });
+        } else {
+          res.redirect('/login');
+        }
+      }
+    });
+
 
 });
 
